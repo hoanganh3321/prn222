36424a186dffce06187ef6a5bea9b4ed2d41da43
hoanganh3321 <hoanganhfallinginlove@gmail.com>
2025-01-16 20:30:33 +0700

    update prn222

---
+++ ServerApp/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void connectedServer(string server, int port)
+        {
+            int bytes;
+            string message, responsedData;
+            try
+            {
+                TcpClient tcpClient = new TcpClient(server, port);
+                Console.Title = "client application";
+                NetworkStream stream = null;
+                while (true)
+                {
+                    Console.WriteLine("input message <press enter to exit> : ");
+                    message = Console.ReadLine();
+                    if (message == string.Empty)
+                    {
+                        break;
+                    }
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes($"{message}");
+                    stream = tcpClient.GetStream();
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("sent:{0}", message);
+                    data = new Byte[256];
+                    bytes = stream.Read(data, 0, data.Length);
+                    responsedData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("recevied:{0}", responsedData);
+
+                }
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception:{0}", e.Message);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string server = "127.0.0.1";
+            int port = 13000;
+            connectedServer(server, port);
+        }
+
+    }
+
+}
---
+++ ServerApp/ServerApp/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApp
+{
+     class Program
+    {
+        static void ProcessMessage(object parm)
+        {
+            string data;
+            int count;
+            try
+            {
+                TcpClient client = parm as TcpClient;
+                byte[] bytes = new Byte[256];
+                NetworkStream stream = client.GetStream();
+                while ((count = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+                    Console.WriteLine($"Received:{data} at {DateTime.Now:t}");
+                    data = $"{data.ToUpper()}";
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine($"Sent:{data}");
+                }
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+                Console.WriteLine("waiting message....");
+            }
+        }
+
+        static void ExecuteServer(string host, int port)
+        {
+            int count = 0;
+            TcpListener server = null;
+            try
+            {
+                Console.Title = "server application";
+                IPAddress localAddress = IPAddress.Parse(host);
+                server = new TcpListener(localAddress, port);
+                server.Start();
+                Console.WriteLine(new string('*',40));
+                Console.WriteLine("waiting for connection........");
+                while(true)
+                {
+                    TcpClient client =server.AcceptTcpClient();
+                    Console.WriteLine($"number of client connected:{++count}");
+                    Console.WriteLine(new string('*',40));
+                    Thread thread=new Thread(new ParameterizedThreadStart(ProcessMessage));
+                    thread.Start(client);
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("exception:{0}",ex.Message);
+
+            }
+            finally
+            {
+                server.Stop();
+                Console.WriteLine("server stopped. press any key to exit");
+            }
+            Console.Read();
+        }
+
+        static void Main()
+        {
+            string host = "127.0.0.1";
+            int port = 13000;
+            ExecuteServer(host, port);
+        }
+
+
+    }
+}
